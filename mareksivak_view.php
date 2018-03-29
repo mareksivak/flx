b4f9be0492af7a00666eecd01d3bcb0bec928fa7
Marek <mmareq@gmail.com>
2018-03-29 13:11:15 +0800

    initial

---
+++ mareksivak_view.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Marek Sivák</title>
+    <meta name="description" content="I design web and mobile interfacess">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#ffffff">
+	<meta property="og:image" content="img/himalayas.png" />
+	<meta property="og:title" content="Discover life changing expeditons | Foundlost" />
+    <meta property="og:description" content="Foundlost is a community where people find and join unique adventures across the planet" />
+    <meta property="og:author" content="Marek Sivák" />
+	
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+      <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW" />
+     <link rel="shortcut icon" href="img/favicon.ico?v=NmmWm0kPj0"> 
+      
+
+    <script src="https://use.typekit.net/gbi7mfb.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+      
+      
+      
+	<link rel="stylesheet" href="css/anims/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/anims/animate-customized.css" type="text/css" />
+	
+    
+    <link rel="stylesheet" href="css/flexboxgrid.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/core.css?ver=2.1" type="text/css" />
+    <link rel="stylesheet" href="css/responsive.css?ver=2" type="text/css" />
+
+    <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+</head>
+<body class="container">
+    <div class="hero">
+        
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-0 no-padding">
+                <div class="head wow fadeIn" data-wow-delay="0s">
+                    <h1>Discover life changing expeditons</h1>
+                    <div class="subhead">Foundlost is a community where people find and join unique adventures across the planet</div>
+                    <!--<div class="update">
+                        <span>Available for freelance. <a href="mailto:xsivak@gmai1.com?subject=Hey Marek!">Reach out</a> to me.</span></div>-->
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="content">
+        <div class="section">
+            <div class="row ">
+                <div class="col-xs-12 col-sm-10 col-md-8"><img class="left-img"  src="img/nomads.png" alt="Pathship" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4 block-desktop wow fadeIn parallax" data-wow-delay="0.25s">
+                    <h5>Footsteps of the Nomads</h5>
+                    <div class="sub-small"><span class="tag">Mongolia — Jun 8-22, 2018</span></div>
+                    <p>Retrace the footsteps of ancient nomadic communities who trekked from Northern China through to Europe.</p>
+                    <div class="row action">
+                        <a class="button" href="https://pathship.com" target="_blank">Explore</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="row">
+                <div class="col-xs-12 col-sm-10 col-md-7 col-md-offset-1"><img class="parallax" src="img/himalayas.png" style="box-shadow: rgba(0, 0, 0, 0.34) 0px 4px 58px" alt="FreeLunchCI" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4 first-md block-desktop wow fadeIn" data-wow-delay="0.25s">
+                    <h5>Tracking to Everest Basecamp</h5>
+                    <div class="sub-small"><span class="tag">Nepal — Sep 18 - Oct 8, 2018</span></div>
+                    <p>The traditional route of Everest Base Camp trek begins from Jiri. While travelling to Jiri you can enjoy the natural beauty on the way, green terraced farm, people rafting in the Bhote Koshi River, many waterfalls and traditional villages on the way to Jiri.</p>
+                    <div class="row action">
+                        <a class="button" href="https://freelunchci.com" target="_blank">Visit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="section">
+            <div class="row ">
+                <div class="col-xs-12 col-sm-10 col-md-7"><img class="" src="img/nomads-merged.png" alt="Pathship" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4 col-md-offset-1 block-desktop wow fadeIn parallax" data-wow-delay="0.25s">
+                    <h5>Footsteps of the Nomads</h5>
+                    <div class="sub-small"><span class="tag">Mongolia — Jun 8-22, 2018</span></div>
+                    <p>Retrace the footsteps of ancient nomadic communities who trekked from Northern China through to Europe.</p>
+                    <div class="row action">
+                        <a class="button" href="https://pathship.com" target="_blank">Explore</a>
+                    </div>
+                </div>
+            </div>
+        </div>    
+                
+        <div class="section">
+            <div class="row">
+                <div class="col-xs-12 col-sm-10 col-md-7 col-md-offset-1"><img class="parallax" src="img/himalayas-merged.png" alt="FreeLunchCI" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4  first-md block-desktop wow fadeIn" data-wow-delay="0.25s">
+                    <h5>Tracking to Everest Basecamp</h5>
+                    <div class="sub-small"><span class="tag">Nepal — Sep 18 - Oct 8, 2018</span></div>
+                    <p>The traditional route of Everest Base Camp trek begins from Jiri. While travelling to Jiri you can enjoy the natural beauty on the way, green terraced farm, people rafting in the Bhote Koshi River, many waterfalls and traditional villages on the way to Jiri.</p>
+                    <div class="row action">
+                        <a class="button" href="https://freelunchci.com" target="_blank">Visit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="section">
+            <div class="row center-xs">
+                <div class="col-xs-12 col-sm-10 col-md-8"><img class="parallax" src="img/levice-kedysi-a-dnes.jpg" alt="Levice: Kedysi a dnes" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4 first-md block-desktop wow fadeIn" data-wow-delay="0.25s">
+                    <h5>Levice: Kedysi a dnes</h5>
+                    <div class="sub-small"><span class="tag">Web</span> <span class="tag">Design</span> <span class="tag">Dev</span></div>
+                    <p>Project comparing historic photos of my hometown with views from Google maps.</p> 
+                    <div class="row action">
+                        <a class="button" href="http://levicekedysiadnes.sk" target="_blank">Visit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="row center-xs">
+                <div class="col-xs-12 col-sm-10 col-md-8"><img class="parallax" src="img/latin-phrases.jpg" alt="Latin Phrases" /></div>
+                <div class="col-xs-12 col-sm-8 col-md-4 block-mobile wow fadeIn" data-wow-delay="0.25s">
+                    <h5>Latin Phrases</h5>
+                    <div class="sub-small"><span class="tag">Android</span><span class="tag">Design</span>  <span class="tag">Dev</span></div>
+                    <p>My first Andorid app, first published in 2013. Became viral and is still growing with 1,000 new users per month.</p>
+                    <div class="row action">
+                        <a class="button" href="https://play.google.com/store/apps/details?id=blowingnose.latin" target="_blank">Google Play</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        <div class="about">
+            <div class="row">
+                <div class="col-xs-10 col-md-8 col-md-offset-2 box wow fadeIn" data-wow-delay="0.25s">
+                    <p><!--I am designer with a development background from Slovakia, now based in Hong Kong. I love working on side projects where I can focus on new challenges.--></p>
+                </div>
+            </div>
+        </div>
+        <!-- Slnko jasno svieti nám vždy do okien -->    
+        <div class="contact wow fadeIn" data-wow-delay="0.5s">
+            <div class="row center-xs">
+                <div class="col-xs-12">
+                    <a href="mailto:xsivak@gmail.com">Contact me <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#000000" d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z" />
+</svg></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+    
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/parallax.js"></script>
+    <script type="text/javascript" src="js/init.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-88646169-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</body>
+</html>
